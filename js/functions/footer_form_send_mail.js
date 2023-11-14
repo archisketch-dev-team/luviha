19edcba01d34d564aff7d34d0ca7ce88c7108618
quangminh-archisketch <quangminh@archisketch.com>
2023-11-14 13:54:40 +0700

    design ui softek

---
+++ js/functions/footer_form_send_mail.js
@@ -0,0 +1,68 @@
+function submitSendEmail() {
+  var emailInput = document.getElementById('email')
+  var emailValue = emailInput.value
+  var errorMessage = document.getElementById('errorMessage')
+
+  // Clear previous error message and reset border color
+  errorMessage.textContent = ''
+  emailInput.style.borderColor = ''
+
+  // Simple email validation
+  if (!emailValue) {
+    errorMessage.textContent = 'Please complete this required field.'
+    emailInput.style.borderColor = '#ef6b51'
+    return
+  }
+
+  if (!validateEmail(emailValue)) {
+    errorMessage.textContent = 'Email must be formatted correctly.'
+    emailInput.style.borderColor = '#ef6b51'
+    return
+  }
+
+  // Hide elements on successful submission
+  emailInput.style.display = 'none'
+  document.querySelector('.footer_button_input_sendMail').style.display = 'none'
+  errorMessage.style.display = 'none'
+
+  // Create and append success message
+  var successMessage = document.createElement('div')
+  successMessage.className = 'submitted-message hs-main-font-element'
+  successMessage.textContent = 'Thanks for submitting the form.'
+
+  var formTarget = document.getElementById('hs_form_target_footer_footer_subscribe_form')
+  formTarget.appendChild(successMessage)
+}
+
+$(document).ready(function () {
+  $('.email').on('input', function () {
+    validateEmailOnChange(this)
+  })
+})
+
+function validateEmailOnChange(input) {
+  var emailValue = input.value
+  var errorMessageId = input.id + 'Error' // Generate error message ID dynamically
+  var errorMessage = document.getElementById(errorMessageId)
+
+  // Clear previous error message and reset border color
+  errorMessage.textContent = ''
+  input.style.borderColor = ''
+
+  // Simple email validation
+  if (!emailValue) {
+    errorMessage.textContent = 'Please complete this required field.'
+    input.style.borderColor = '#ef6b51'
+    return
+  }
+
+  if (!validateEmail(emailValue)) {
+    errorMessage.textContent = 'Email must be formatted correctly.'
+    input.style.borderColor = '#ef6b51'
+    return
+  }
+}
+
+function validateEmail(email) {
+  return /\S+@\S+\.\S+/.test(email)
+}
